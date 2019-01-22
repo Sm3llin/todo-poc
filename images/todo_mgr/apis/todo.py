ebd269a433a1681064fadcf0a46e28e52e3c9a66
michaelellin <michaelellin@gmail.com>
2019-01-23 05:52:52 +1100

    rest: changed puts to posts, consuming events each run

--- images/todo_mgr/apis/todo.py
+++ images/todo_mgr/apis/todo.py
@@ -3,9 +3,9 @@ from flask_restplus import Namespace, Resource, fields
 from db import session
 from models.todo import Todo as TodoModel
 
-api = Namespace('todo', description='Todo List management')
+api = Namespace("todo", description="Todo List management")
 
-todo = api.model('Todo', {
+todo = api.model("Todo", {
     "id": fields.String(required=True, attribute="todo_id", description="the Todo identifier"),
     "title": fields.String(description="the Todo item title"),
     "description": fields.String(description="the Todo item description"),
@@ -13,8 +13,8 @@ todo = api.model('Todo', {
 
 
 todo_parser = api.parser()
-todo_parser.add_argument('title', type=str, help='title to display summarise the Todo', required=True)
-todo_parser.add_argument('description', type=str, help='brief description of Todo')
+todo_parser.add_argument("title", type=str, help='title to display summarise the Todo', required=True)
+todo_parser.add_argument("description", type=str, help='brief description of Todo')
 
 update_parser = todo_parser.copy()
 update_parser.replace_argument("title", type=str, help='title to display summarise the Todo', required=False)
@@ -32,7 +32,7 @@ class TodoList(Resource):
     @api.expect(todo_parser)
     @api.response(409, "Todo not created")
     @api.marshal_with(todo)
-    def put(self):
+    def post(self):
         """create a new Todo"""
         args = todo_parser.parse_args()
 
