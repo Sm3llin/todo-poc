9b27ad94077e99546d6c875135db86b2ec707a8f
michaelellin <michaelellin@gmail.com>
2019-01-23 00:24:58 +1100

    rest: added in put command to create a new Todo

--- images/todo_mgr/apis/todo.py
+++ images/todo_mgr/apis/todo.py
@@ -1,15 +1,20 @@
 from flask_restplus import Namespace, Resource, fields
 
 from db import session
-from models.todo import Todo
+from models.todo import Todo as TodoModel
 
-api = Namespace('todos', description='Todo List management')
+api = Namespace('todo', description='Todo List management')
 
 todo = api.model('Todo', {
     "id": fields.String(required=True, attribute="todo_id", description="The Todo identifier"),
     "title": fields.String(description="The Todo item title"),
     "description": fields.String(description="The Todo item description"),
 })
+
+
+todo_parser = api.parser()
+todo_parser.add_argument('title', type=str, help='Rate cannot be converted', required=True)
+todo_parser.add_argument('description', type=str, help='Rate cannot be converted')
 
 
 @api.route('/')
@@ -20,6 +25,20 @@ class TodoList(Resource):
         """List all todos"""
         return session.query(Todo).all()
 
+    @api.doc("create_todo")
+    @api.expect(todo_parser)
+    @api.response(409, "Todo not created")
+    @api.marshal_with(todo)
+    def put(self):
+        """Create a new Todo"""
+        args = todo_parser.parse_args()
+
+        _todo = TodoModel.create(**args)
+
+        if _todo:
+            return _todo
+        api.abort(409, "unable to create Todo")
+
 
 @api.route('/<id>')
 @api.param('id', 'The todo identifier')
@@ -29,7 +48,7 @@ class Todo(Resource):
     @api.marshal_with(todo)
     def get(self, id):
         """Fetch a todo given its identifier"""
-        _todo = session.query(Todo).get(id)
+        _todo = session.query(TodoModel).get(id)
 
         if _todo:
             return _todo
